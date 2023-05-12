e567297914c3cabd325a3b8c2c15326a4efe633f
CheffJeff <1999jeffreynijkamp@gmail.com>
2023-05-12 14:37:40 +0200

    update header

--- dashboard/src/router/index.ts
+++ dashboard/src/router/index.ts
@@ -6,7 +6,7 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: 'signIn',
       component: SignInView
     },
     {
@@ -16,7 +16,12 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
-    }
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: () => import('../views/HomeView.vue')
+    },
   ]
 })
 
--- dashboard/src/views/HomeView.vue
+++ dashboard/src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import Header from '../components/Header.vue';
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <Header/>
+    <div class="page-wrap">
+      <h1>Hello there</h1>
+    </div>
+  </div>
 </template>
