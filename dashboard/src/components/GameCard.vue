8d49a536acafe5815a9081cf7b6525f1d276d98a
CheffJeff <1999jeffreynijkamp@gmail.com>
2023-05-22 14:34:52 +0200

    add cards

---
+++ dashboard/src/components/GameCard.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+defineProps({
+  Descriptin: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="card">
+    <div class="inner">
+      <div class="img-wap">
+        <img src="https://placehold.co/600x400" alt="game-img">
+      </div>
+      <p>{{ Descriptin }}</p>  
+      <a class="btn btn-main mt-4">
+        <span>
+          View
+        </span>
+      </a>   
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "../assets/styles/components/game-card.scss";
+</style>
--- dashboard/src/views/HomeView.vue
+++ dashboard/src/views/HomeView.vue
@@ -1,13 +1,54 @@
 <script setup lang="ts">
 import Header from '../components/Header.vue';
-
+import GameCard from '../components/GameCard.vue';
 </script>
 
 <template>
-  <div>
+  <section>
     <Header/>
     <div class="page-wrap">
-      <h1>Hello there</h1>
+      <div class="container-fluid pacer">
+        <div class="inner-wrap">
+          <div class="btn-wrap">
+            <a class="btn btn-main mt-4">
+              <span>
+                Start new game
+              </span>
+            </a>
+            <a class="btn btn-main mt-4">
+              <span>
+                Add Questions
+              </span>
+            </a>
+          </div>
+          <section class="currgames game-slide">
+            <h1>
+              Ongoing games
+            </h1>
+            <div class="slide">
+              <GameCard Descriptin="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum sed quidem, aliquid delectus atque ab, eligendi accusantium adipisci molestiae odit dolor aspernatur? Accusamus fugit est pariatur dolor porro explicabo mollitia ea placeat totam qui, sint cum tempore. Corrupti hic fugit, accusamus deleniti, nobis laboriosam minus quisquam sapiente tempora, quidem rem!" />
+              <GameCard Descriptin="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum sed quidem, aliquid delectus atque ab, eligendi accusantium adipisci molestiae odit dolor aspernatur? Accusamus fugit est pariatur dolor porro explicabo mollitia ea placeat totam qui, sint cum tempore. Corrupti hic fugit, accusamus deleniti, nobis laboriosam minus quisquam sapiente tempora, quidem rem!" />
+              <GameCard Descriptin="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum sed quidem, aliquid delectus atque ab, eligendi accusantium adipisci molestiae odit dolor aspernatur? Accusamus fugit est pariatur dolor porro explicabo mollitia ea placeat totam qui, sint cum tempore. Corrupti hic fugit, accusamus deleniti, nobis laboriosam minus quisquam sapiente tempora, quidem rem!" />
+              <GameCard Descriptin="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum sed quidem, aliquid delectus atque ab, eligendi accusantium adipisci molestiae odit dolor aspernatur? Accusamus fugit est pariatur dolor porro explicabo mollitia ea placeat totam qui, sint cum tempore. Corrupti hic fugit, accusamus deleniti, nobis laboriosam minus quisquam sapiente tempora, quidem rem!" />
+            </div>
+          </section>
+          <section class="games game-slide">
+            <h2>
+              Ended games
+            </h2>
+            <div class="slide">
+              <GameCard Descriptin="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum sed quidem, aliquid delectus atque ab, eligendi accusantium adipisci molestiae odit dolor aspernatur? Accusamus fugit est pariatur dolor porro explicabo mollitia ea placeat totam qui, sint cum tempore. Corrupti hic fugit, accusamus deleniti, nobis laboriosam minus quisquam sapiente tempora, quidem rem!" />
+              <GameCard Descriptin="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum sed quidem, aliquid delectus atque ab, eligendi accusantium adipisci molestiae odit dolor aspernatur? Accusamus fugit est pariatur dolor porro explicabo mollitia ea placeat totam qui, sint cum tempore. Corrupti hic fugit, accusamus deleniti, nobis laboriosam minus quisquam sapiente tempora, quidem rem!" />
+              <GameCard Descriptin="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum sed quidem, aliquid delectus atque ab, eligendi accusantium adipisci molestiae odit dolor aspernatur? Accusamus fugit est pariatur dolor porro explicabo mollitia ea placeat totam qui, sint cum tempore. Corrupti hic fugit, accusamus deleniti, nobis laboriosam minus quisquam sapiente tempora, quidem rem!" />
+              <GameCard Descriptin="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum sed quidem, aliquid delectus atque ab, eligendi accusantium adipisci molestiae odit dolor aspernatur? Accusamus fugit est pariatur dolor porro explicabo mollitia ea placeat totam qui, sint cum tempore. Corrupti hic fugit, accusamus deleniti, nobis laboriosam minus quisquam sapiente tempora, quidem rem!" />
+            </div>
+          </section>
+        </div>
+      </div>
     </div>
-  </div>
+  </section>
 </template>
+
+<style lang="scss" scoped>
+  @import "../assets/styles/pages/home.scss";
+</style>
