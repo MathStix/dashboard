0fa54757af8f30febd037f26d6a518e3f60ea2d7
CheffJeff <1999jeffreynijkamp@gmail.com>
2023-05-31 15:04:45 +0200

    update

--- dashboard/src/assets/javascript/models/exercise.ts
+++ dashboard/src/assets/javascript/models/exercise.ts
@@ -1,28 +1,28 @@
 export interface ExerciseInterface {
-  _id: number,
+  _id: number | null,
   title: string,
   description: string,
   answer: string,
   location: string,
-  photo: string,
+  photo: string | null,
   activationRange: string,
   exerciseType: string,
-  teacherId: number | null
+  teacherId: string | null
 }
 
 export class Exercise implements ExerciseInterface {
-  _id: number;
+  _id: number | null;
   title: string;
   description: string;
   answer: string;
   location: string;
-  photo: string;
+  photo: string | null;
   activationRange: string;
   exerciseType: string;
-  teacherId: number | null;
+  teacherId: string | null;
   
-  constructor(id: number, title: string, description: string, answer: string, 
-    location: string, photo: string, activation: string, type: string, teacherId: number | null) {
+  constructor(id: number | null, title: string, description: string, answer: string, 
+    location: string, photo: string | null, activation: string, type: string, teacherId: string | null) {
       this._id = id;
       this.title = title;
       this.description = description;
@@ -37,8 +37,22 @@ export class Exercise implements ExerciseInterface {
   getImage = ():string  => {
     const img = new Image()
     img.src = "data:image/png;base64," + this.photo
-
     return img.src
+  }
+
+  genarateBase64 = (file: any) => {
+    console.log(file)
+    const reader = new FileReader()
+    reader.onloadend = () => {
+      
+      const base64String = (<string>reader.result)
+        .replace('data:', '')
+        .replace(/^.+,/, '');
+
+      this.photo = base64String
+    };
+    reader.readAsDataURL(file);
+
   }
 }
 
